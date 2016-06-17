63aab2a09911f7163f5f72a06a1e146538593415
System Administrator <root@Neils-MacBook-Pro.local>
2016-06-17 16:04:14 +0800

    Add the steps to learn for statement

--- firstApp/shell_learning.sh
+++ firstApp/shell_learning.sh
@@ -1,2 +1,23 @@
 # this is to learn the shell programing
+# the test functionalities
+val1='Hello World'
+echo $val1
+echo "$val1"
+if [ -n "$val1" ]
+then
+  echo "$val1 is not empty"
+fi
 
+if test '$val1'=''
+then
+ echo "'$val1' is not empty"
+fi
+
+# iterate the folder items
+for file in /Users/neilwang/*; do
+  if [ -d $file ]; then
+    echo "$file is a directory!"
+  elif [ -f $file ]; then
+    echo "$file is a file!"
+  fi
+done
