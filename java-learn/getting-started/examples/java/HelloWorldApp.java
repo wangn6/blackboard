463a1a3c1226f27c0bab1b12781ff0c7628ed345
System Administrator <root@Neils-MacBook-Pro.local>
2016-09-17 09:55:52 +0800

    Add the first java application HelloWorld

---
+++ java-learn/getting-started/examples/java/HelloWorldApp.java
@@ -0,0 +1,10 @@
+/* *
+* The HelloWorldApp class implement an application that simple prints "Hello World!" to standard output
+*
+*/
+class HelloWorldApp{
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+
+}
