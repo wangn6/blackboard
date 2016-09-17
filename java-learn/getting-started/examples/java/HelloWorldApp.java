8682ac697fd64ba6f6d2ed3605c909b0ef6ba0b2
System Administrator <root@Neils-MacBook-Pro.local>
2016-09-17 10:29:33 +0800

    Change the class to public to verify javadoc works as expected

--- java-learn/getting-started/examples/java/HelloWorldApp.java
+++ java-learn/getting-started/examples/java/HelloWorldApp.java
@@ -2,7 +2,7 @@
 * The HelloWorldApp class implement an application that simple prints "Hello World!" to standard output
 *
 */
-class HelloWorldApp{
+public class HelloWorldApp{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 	}
