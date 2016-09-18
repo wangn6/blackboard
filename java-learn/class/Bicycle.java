71ec347d7f3af92c167fa0576509336428b48f05
System Administrator <root@Neils-MacBook-Pro.local>
2016-09-18 23:03:13 +0800

    Create the first class Bicycle

---
+++ java-learn/class/Bicycle.java
@@ -0,0 +1,32 @@
+public class Bicycle
+{
+	public int gear;
+	public int speed;
+	public int cadence;
+	public Bicycle(int startCadence, int startSpeed, int startGear){
+		gear = startGear;
+		cadence = startCadence;
+		speed = startSpeed;
+	}
+	public void setCadence( int newValue)
+	{
+		gear = newValue;
+	}
+	public void setSpeed( int newValue)
+	{
+		speed = newValue;
+	}
+	public void setGear(int newValue)
+	{
+		gear = newValue;
+	}
+	public void print()
+	{
+		System.out.println("Cadence:"+cadence+";Speed:"+speed+";Gear:"+gear);	
+	}
+	public static void main(String[] args)
+	{
+		Bicycle bike = new Bicycle(1,2,3);
+		bike.print();
+	}
+}
